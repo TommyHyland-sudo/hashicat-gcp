ab0d0715c84b33ef5cea36741a07d68fc765659e
Thomas Hyland <thyland1999@gmail.com>
2023-03-28 07:03:31 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+    source  = "app.terraform.io/training-organisation/network/google"
+    version = "3.4.0"
+
+    project_id   = "p-o1j9sqaze7on-0"
+    network_name = "example-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name   = "gaurav-subnet"
+            subnet_ip     = "10.100.10.0/24"
+            subnet_region = var.region
+        }
+    ]
+}
